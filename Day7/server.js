cd47d369e91bc242fe7798942bfabdbb78c8071a
Eduard Cristea <73955627+eduardcristea@users.noreply.github.com>
2021-05-10 17:59:56 +0300

    Update server.js

--- Day7/server.js
+++ Day7/server.js
@@ -7,7 +7,7 @@ const app = express()
 const port = 3000
 app.get('/', (req, res) =>{
 var con = mysql.createConnection({
- host: 'localhost' ,
+ host: 'eschiuel' ,
  user: 'eduard' ,
  password: '8d3cc543' ,
  database: 'test' 
