aaa5ff99ce0a2e9deaae1f120e052791cb3b8cd8
Eduard Cristea <73955627+eduardcristea@users.noreply.github.com>
2021-05-12 15:40:24 +0300

    Update server.js

--- Day8/server.js
+++ Day8/server.js
@@ -5,6 +5,5 @@ process.on('uncaughtException', (err) => {
 const express = require('express')
 const app = express()
 const port = 3000
-app.get('/', (req, res) =>{
- })
+app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
